62ba458772b1c302a5cf85f974cb662ca5214844
xerocross <adam.cross.inc@gmail.com>
2018-07-30 17:06:03 -0400

    setup npm package with basic functionality and add some testing using jest

--- we-assert.js
+++ we-assert.js
@@ -1,48 +1,72 @@
-var levels = {
-    0 : "DEBUG",
-    1 : "WARN",
-    2 : "ERROR"
-}
-var levelStringToInt = function(levelString) {
-  switch (levelString) {
-    case "DEBUG":
-      return 0;
-      break;
-    case "WARN":
-      return 1;
-      break;
-    case "ERROR":
-      return 2;
-      break;
-  }
-}
-var currentLevel = 2;
-var setLevel = function(levelString) {
-  var newLevel = levelStringToInt(levelString);
-  if (newLevel) {
-    currentLevel = newLevel;
-  }
-}
-
-var getLevel = function() {
-  return levels(currentLevel);
-}
-
-var that = function(statement, message) {
-    if (!statement) {
-      throw new Error("false assertion: " + message);
+var buildWeAssert = function() {
+  var levels = {
+      0 : "DEBUG",
+      1 : "WARN",
+      2 : "ERROR"
+  };
+  var levelStringToInt = function(levelString) {
+    switch (levelString) {
+      case "DEBUG":
+        return 0;
+        break;
+      case "WARN":
+        return 1;
+        break;
+      case "ERROR":
+        return 2;
+        break;
     }
-};
-var internalVerification = function(levelString, verificationFunction) {
-  let level = levelStringToInt(levelString);
-  if (level <= currentLevel) {
-    verificationFunction();
+  };
+  var currentLevel = 2;
+  var setLevel = function(levelString) {
+    var newLevel = levelStringToInt(levelString);
+    if (newLevel == 0 || newLevel == 1 || newLevel == 2) {
+      currentLevel = newLevel;
+    } else {
+      throw "unexpected level"
+    }
+  };
+
+  var getLevel = function() {
+    return levels[currentLevel];
+  };
+
+  var handler = function() {};
+  var setHandler = function(newHandler) {
+    handler = newHandler;
+  };
+
+  var that = function(statement, message) {
+      if (!statement) {
+        handler(statement, message);
+      }
+  };
+  var internalVerification = function(levelString, verificationFunction) {
+    let level = levelStringToInt(levelString);
+    if (level <= currentLevel) {
+      verificationFunction();
+    }
+  };
+
+  var atLevel = function(someLevelString) {
+    var obj = {};
+    obj.that = function(statement, message) {
+      let level = levelStringToInt(someLevelString);
+      if (level >= currentLevel) {
+        that(statement, message);
+      } else {
+
+      }
+    }
+    return obj;
+  }
+
+  return {
+    setLevel : setLevel,
+    that : that,
+    atLevel: atLevel,
+    setHandler : setHandler,
+    getLevel: getLevel
   }
 }
-
-module.exports.setLevel = setLevel;
-
-module.exports.weAssert = {
-  that : that
-}
-module.exports.verify = internalVerification;
+module.exports.buildWeAssert = buildWeAssert;
